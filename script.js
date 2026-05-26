d24f649590ecbc91e5098ead9580a3c4f4f3b333
jannahpitogo <pitogojannah@outlook.com>
2026-05-26 12:38:33 +0200

    refactor: update input handling and display user name on chat page

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const submitName = document.getElementById("submit-name");
 const firstPage = document.getElementById("first-page");
 const chatPage = document.getElementById("chat-page");
+const userName = document.getElementById("name-display");
 
 submitName.addEventListener("click", (event) => {
   event.preventDefault();
   firstPage.style.display = "none";
   chatPage.style.display = "block";
+
+  const nameInputArea = document.getElementById("name-input").value;
+  userName.textContent = `Welcome to the chat ${nameInputArea}`;
 });
